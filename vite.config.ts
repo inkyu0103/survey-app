b020a5b7d7e9c9b9d731189704716d00fa4f090b
inkyu <inkyu0103@gmail.com>
2025-07-24 10:53:58 +0900

    chore: route

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.NODE_ENV === "production" ? "/survey-app/" : "/",
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
